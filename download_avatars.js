cd23edd73b22ae018b6272fa470c060fbfaaeece
Tory Shoreman <toryshoreman@gmail.com>
2017-02-14 00:21:13 +0000

    Adding solution to steps 1 to 4

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,27 @@
 var request = require('request');
+var GITHUB_USER = "torshore";
+var GITHUB_TOKEN = "cdedbdb0ced15f7e64f1907dfc6225af4eb33aa8";
 
-console.log('Welcome to the GitHub Avatar Downloader');
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+   var options = {
+     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+     headers: {
+       'User-Agent': 'request'
+     }
+   };
+   request.get(options, function (error, response, body) {
+     var data = JSON.parse(body);
+     if (!error && response.statusCode == 200) {
+        cb(data.forEach(function(user) {
+        console.log(user.avatar_url);
+      }));
+     }
+  });
+}
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+})
+
