022638de8cc20cd99d98b8651fdad56331eea0a0
Tory Shoreman <toryshoreman@gmail.com>
2017-02-14 01:02:00 +0000

    Submitting Solution to Step 6

--- download_avatars.js
+++ download_avatars.js
@@ -4,24 +4,27 @@ var GITHUB_TOKEN = "cdedbdb0ced15f7e64f1907dfc6225af4eb33aa8";
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+
    var options = {
      url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
      headers: {
        'User-Agent': 'request'
      }
    };
+
    request.get(options, function (error, response, body) {
-     var data = JSON.parse(body);
+
      if (!error && response.statusCode == 200) {
-        cb(data.forEach(function(user) {
-        console.log(user.avatar_url);
-      }));
+        cb(error, body);
      }
   });
 }
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
-})
-
+  var data = JSON.parse(result);
+  var array = [];
+  data.forEach(function(user) {
+    array.push(user.avatar_url);
+});
+  console.log(array);
+});
