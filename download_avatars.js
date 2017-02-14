898eca0045e5eb1778ae1358c8d9560b927036cf
Tory Shoreman <toryshoreman@gmail.com>
2017-02-14 01:19:51 +0000

    Adding Solution to Step 7 with filepath error

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var GITHUB_USER = "torshore";
 var GITHUB_TOKEN = "cdedbdb0ced15f7e64f1907dfc6225af4eb33aa8";
 
@@ -28,3 +29,10 @@ getRepoContributors("jquery", "jquery", function(err, result) {
 });
   console.log(array);
 });
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .pipe(fs.createWriteStream('./${filepath}.jpg'));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./kvirani.jpg")
