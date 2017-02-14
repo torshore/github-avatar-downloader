0138197a9c34eea573a9a0ad0e475f2fc2cf0ca0
Tory Shoreman <toryshoreman@gmail.com>
2017-02-14 02:37:38 +0000

    Adding Step 9, process.argv

--- download_avatars.js
+++ download_avatars.js
@@ -2,10 +2,12 @@ var request = require('request');
 var fs = require('fs');
 var GITHUB_USER = "torshore";
 var GITHUB_TOKEN = "cdedbdb0ced15f7e64f1907dfc6225af4eb33aa8";
+var arg1 = process.argv.slice(2, 3)[0]; //repo owner
+var arg2 = process.argv.slice(3, 4)[0]; //repo name
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
+  //options is necessary for github access (400 error will be received without)
    var options = {
      url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
      headers: {
@@ -28,12 +30,11 @@ function downloadImageByURL(url, filePath) {
   .pipe(fs.createWriteStream(filePath));
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(arg1, arg2, function(err, result) {
   var data = JSON.parse(result);
   data.forEach(function(user) {
     downloadImageByURL(user.avatar_url, "./Avatars/" + user.login + ".jpg");
    });
-
 });
 
 
